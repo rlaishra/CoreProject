6a4aa337614e7db2bf551108163b69cd75d90ccd
Ricky <rlaishra@syr.edu>
2017-07-18 15:08:53 -0400

    Print current file

--- src/kcore_experiments.py
+++ src/kcore_experiments.py
@@ -14,5 +14,7 @@ if os.path.exists(fpath) and os.path.exists(spath):
 			fname = os.path.join(fpath, i + '_' + str(v) + '.csv')
 			sname = os.path.join(spath, i + '_' + str(v))
 
+			print('Processing: {}'.format(fname))
+
 			kcore_exp = kcore.KCoreExperiment(fname, sname, False, '010')
 			kcore_exp.runExperiment(50, 1, 51)
