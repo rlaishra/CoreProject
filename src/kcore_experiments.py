d442737cdb30f60863f257dbbdd5309989ccf72b
Ricky <rlaishra@syr.edu>
2017-07-18 15:07:31 -0400

    K core experiments file

---
+++ src/kcore_experiments.py
@@ -0,0 +1,18 @@
+from experiments import kcore
+import sys
+import os
+
+identifiers = ['p2p09', 'hamster']
+vals = range(0, 11)
+
+fpath = sys.argv[1]
+spath = sys.argv[2]
+
+if os.path.exists(fpath) and os.path.exists(spath):
+	for i in identifiers:
+		for v in vals:
+			fname = os.path.join(fpath, i + '_' + str(v) + '.csv')
+			sname = os.path.join(spath, i + '_' + str(v))
+
+			kcore_exp = kcore.KCoreExperiment(fname, sname, False, '010')
+			kcore_exp.runExperiment(50, 1, 51)
